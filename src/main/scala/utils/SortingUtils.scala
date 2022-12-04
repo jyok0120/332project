bc1a5a3d8585e9bebf8a4ed0fd19fc00fa9fad0b
Hyunji Song <dkhyunji19@gmail.com>
2022-12-04 22:23:19 +0900

    Fix: Compatibility issues in SortingUtils.scala

--- src/main/scala/utils/SortingUtils.scala
+++ src/main/scala/utils/SortingUtils.scala
@@ -1,171 +1,126 @@
 package utils
 
 import org.apache.logging.log4j.scala.Logging
-import java.io._
-
-import java.util.*;
+import scala.collection._
+import scala.concurrent.{Future, ExecutionContext}
+import scala.math._
  
 // Goal: offer a generic external-memory sorting program in Java.
- 
+
 object SortingUtils extends Logging {
- 
-     // This will simply load the file by blocks of x rows, then
-     // sort them in-memory, and write the result to a bunch of 
-     // temporary files that have to be merged later.
-     
-     // @param file some flat  file
-     // @return a list of temporary flat files
- 
-    public static List<File> sortInBatch(File file, Comparator<String> cmp) throws IOException {
-        List<File> files = new ArrayList<File>();
-        BufferedReader fbr = new BufferedReader(new FileReader(file));
-        long blocksize = estimateBestSizeOfBlocks(file);// in bytes
-        try{
-            List<String> tmplist =  new ArrayList<String>();
-            String line = "";
-            try {
-                while(line != null) {
-                    long currentblocksize = 0;// in bytes
-                    while((currentblocksize < blocksize) 
-                    &&(   (line = fbr.readLine()) != null) ){ // as long as you have 2MB
-                        tmplist.add(line);
-                        currentblocksize += line.length() // 2 + 40; // java uses 16 bits per character + 40 bytes of overhead (estimated)
-                    }
-                    files.add(sortAndSave(tmplist,cmp));
-                    tmplist.clear();
-                }
-            } catch(EOFException oef) {
-                if(tmplist.size()>0) {
-                    files.add(sortAndSave(tmplist,cmp));
-                    tmplist.clear();
-                }
+    /*
+    def splitText(input: Array[Array[Byte]], output: Array[Array[Byte]]): Array[Array[Byte]] = {
+        input
+    }
+
+    def compare(input: Array[Array[Byte]], output: Array[Array[Byte]])
+    */
+    def sort(input: String, tmpDir: String, output: String, linesPerChunk: Int)
+        (implicit ec: ExecutionContext): Future[String] = {
+
+        // split file into n chunks
+        val chunks = splitStep(input, linesPerChunk, tmpDir)
+
+        // sort each chunk locally, in parallel
+        val sortAsync = sortStep(chunks)
+
+        // merge sorted chunks using k-way merge algorithm
+        val merge = sortAsync map { sorted =>
+            val n = max(1, linesPerChunk / sorted.size) // n should be between 1 to linesPerChunk / sorted.size
+            mergeStep(sorted, output, n)
+        }
+
+        // clean up
+        merge onSuccess { case _ => IO.delete(chunks) }
+
+        merge
+    }
+
+    def splitStep(in: String, linesPerChunk: Int, tmp: String): Seq[String] = {
+        val (handle, lines) = IO.readLines(in)
+        val chunked = lines.grouped(linesPerChunk).zipWithIndex
+
+        val chunks = chunked map { case (chunk, id) =>
+            val out = s"$tmp/chunk-$id"
+            IO.writeSeq(chunk, out, true)
+            out
+        } toList
+
+        handle.close()
+
+        chunks 
+    }
+
+    // sort in memory
+    // must limit the number of instance being processed concurrently to prevent OutOfMemoryException
+    def sortStep(chunks: Seq[String])(implicit ec: ExecutionContext): Future[Seq[String]] = {
+        val async  = chunks map { path => Future(sortChunk(path)) }
+        val joined = Future.sequence(async)
+        joined
+    }
+
+    def sortChunk(path: String): String = {
+        val (handle, lines) = IO.readLines(path)
+        val it = lines.toArray.map(_.toInt)
+        handle.close() // clean up
+        IO.writeSeq(it.sorted.map(_.toString), path, true) // 1 pass to write
+    }
+
+    // using k-way merging algorithm to merge sorted chunks
+    def mergeStep(chunks: Seq[String], out: String, linesPerChunk: Int): String = {
+        // initialize variables - priority queue, file readers
+        val readers = chunks.zipWithIndex map { case (chunk, id) =>
+        val (handle, it) = IO.readLines(chunk)
+        // chunk id is used to idenfity which chunk reader to advance after dequeing an element from the queue
+        val indexed = it map { e => (e, id) }
+        (handle, indexed)
+        } toVector
+        val totalSize = readers.size
+        var closedReaders = mutable.Set[Int]()
+        // the priority queue stores (Int, Int), i.e., the actual value as well as the chunk id where it resides in
+        // a crucial part of the k-way merge algorithm is to advance the reader of a particular chunk reader
+        // after an element has been dequeued from the queue, hence why we store the chunk id along with the actual element
+        val sortedQueue = new ScalaQueue[(Int, Int)](Ordering.by { case (v, i) => -v})
+
+        // initialize the priority queue with the first lines of each chunks
+        val lines = readers flatMap { case (_, it) =>
+            val tmp = it.take(linesPerChunk).map { case (v, i) => (v.toInt, i) }
+            tmp toSeq
+        }
+        lines foreach { e => sortedQueue.enqueue(e) }
+
+        // then, iterate over the rest of the chunks and put values into the priority queue,
+        // before finally writing the sorted data to 'out'
+        IO.overwrite(out) { writer =>
+        while (closedReaders.size < totalSize || sortedQueue.notEmpty) {
+            // dequeue an element 'v1' and write it to 'out'
+            val (e, chunkId) = sortedQueue.dequeue()
+            writer.write(s"$e")
+            writer.newLine()
+
+            // then read the next line from chunk 'chunkId' put them in the priority queue
+            val next = readers(chunkId)._2
+            .map { case (v, i) => (v.toInt, i) }
+            .toSeq
+            next foreach { e => sortedQueue.enqueue(e) }
+
+            // check if chunk 'chunkId' has been fully processed
+            // if it has, close the reader and store the id in 'closedReaders'
+            val it     = readers(chunkId)._2
+            val handle = readers(chunkId)._1
+            if (it.isEmpty) {
+            // close reader and put the id in closedReaders set
+            val alreadyClosed = closedReaders.contains(chunkId)
+            if (!alreadyClosed) {
+                handle.close()
+                closedReaders += chunkId
             }
-        } finally {
-            fbr.close();
-        }
-        return files;
-    }
- 
- 
-    public static File sortAndSave(List<String> tmplist, Comparator<String> cmp) throws IOException  {
-        Collections.sort(tmplist,cmp);  // 
-        File newtmpfile = File.createTempFile("sortInBatch", "flatfile");
-        newtmpfile.deleteOnExit();
-        BufferedWriter fbw = new BufferedWriter(new FileWriter(newtmpfile));
-        try {
-            for(String r : tmplist) {
-                fbw.write(r);
-                fbw.newLine();
+            else {
+                // do nothing, reader is already closed
             }
-        } finally {
-            fbw.close();
-        }
-        return newtmpfile;
-    }
- 
-     // This merges a bunch of temporary flat files 
-     // @param files
-     // @param output file
-     // @return The number of lines sorted. (P. Beaudoin)
- 
-    public static int mergeSortedFiles(List<File> files, File outputfile, final Comparator<String> cmp) throws IOException {
-        PriorityQueue<BinaryFileBuffer> pq = new PriorityQueue<BinaryFileBuffer>(11, 
-            new Comparator<BinaryFileBuffer>() {
-              public int compare(BinaryFileBuffer i, BinaryFileBuffer j) {
-                return cmp.compare(i.peek(), j.peek());
-              }
             }
-        );
-        for (File f : files) {
-            BinaryFileBuffer bfb = new BinaryFileBuffer(f);
-            pq.add(bfb);
         }
-        BufferedWriter fbw = new BufferedWriter(new FileWriter(outputfile));
-        int rowcounter = 0;
-        try {
-            while(pq.size()>0) {
-                BinaryFileBuffer bfb = pq.poll();
-                String r = bfb.pop();
-                fbw.write(r);
-                fbw.newLine();
-                ++rowcounter;
-                if(bfb.empty()) {
-                    bfb.fbr.close();
-                    bfb.originalfile.delete();// we don't need you anymore
-                } else {
-                    pq.add(bfb); // add it back
-                }
-            }
-        } finally { 
-            fbw.close();
-            for(BinaryFileBuffer bfb : pq ) bfb.close();
         }
-        return rowcounter;
+        out
     }
- 
-    public static void main(String[] args) throws IOException {
-        if(args.length<2) {
-            System.out.println("please provide input and output file names");
-            return;
-        }
-        String inputfile = args[0];
-        String outputfile = args[1];
-        Comparator<String> comparator = new Comparator<String>() {
-            public int compare(String r1, String r2){
-                return r1.compareTo(r2);}};
-        List<File> l = sortInBatch(new File(inputfile), comparator) ;
-        mergeSortedFiles(l, new File(outputfile), comparator);
-    }
-}
- 
- 
-class BinaryFileBuffer  {
-    public static int BUFFERSIZE = 2048;
-    public BufferedReader fbr;
-    public File originalfile;
-    private String cache;
-    private boolean empty;
-     
-    public BinaryFileBuffer(File f) throws IOException {
-        originalfile = f;
-        fbr = new BufferedReader(new FileReader(f), BUFFERSIZE);
-        reload();
-    }
-     
-    public boolean empty() {
-        return empty;
-    }
-     
-    private void reload() throws IOException {
-        try {
-          if((this.cache = fbr.readLine()) == null){
-            empty = true;
-            cache = null;
-          }
-          else{
-            empty = false;
-          }
-      } catch(EOFException oef) {
-        empty = true;
-        cache = null;
-      }
-    }
-     
-    public void close() throws IOException {
-        fbr.close();
-    }
-     
-     
-    public String peek() {
-        if(empty()) return null;
-        return cache.toString();
-    }
-    public String pop() throws IOException {
-      String answer = peek();
-        reload();
-      return answer;
-    }
-     
-     
- 
 }
